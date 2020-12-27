2cfea3df07faf233d41ae4f52f30e0911bb7879d
felecanvlad <74371101+felecanvlad@users.noreply.github.com>
2020-12-27 13:16:04 +0200

    improve header

--- functions.js
+++ functions.js
@@ -1,4 +1,19 @@
-let activePage = "languages";
+let activePage = "home";
+
+const modal = document.getElementById ('myModal');
+const img = document.getElementById ('myImg');
+const modalImg = document.getElementById ('img01');
+const captionText = document.getElementById ('caption');
+
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt; 
+}
+const span = document.getElementsByClassName ('close') [0];
+span.onclick = function () {
+    modal.style.display = "none";
+}
 
 function hide(id) {
     const el = document.getElementById(id);
